85931902294b9d6d5921fd1440bb4c26f0f3ff14
Phùng Đức Tài <159005177+ductaihjhj@users.noreply.github.com>
2025-01-05 17:03:46 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -2,12 +2,12 @@
 > deleteHeader by ducwtaif
 ***********************************************/
 
-const version = "V1.0.2";
+const version = 'V1.0.2';
 
-function setHeaderValue(e, a, d) {
-  var r = a.toLowerCase();
-  r in e ? (e[r] = d) : (e[a] = d);
+function setHeaderValue(headers, key, value) {
+  headers[key.toLowerCase()] = value;
 }
+
 var modifiedHeaders = $request.headers;
-setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", ""),
-  $done({ headers: modifiedHeaders });
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+$done({ headers: modifiedHeaders });
