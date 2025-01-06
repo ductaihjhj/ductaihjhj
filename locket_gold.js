738410e4004ba055f274f4a1b5828bcded9794f9
Phùng Đức Tài <159005177+ductaihjhj@users.noreply.github.com>
2025-01-06 16:21:58 +0700

    Update locket_gold.js

--- locket_gold.js
+++ locket_gold.js
@@ -16,15 +16,15 @@ var locket = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2024-04-12T01:04:18Z",
-  purchase_date: "2024-04-12T01:04:17Z",
+  original_purchase_date: "2006-09-25T01:04:18Z",
+  purchase_date: "2006-09-25T01:04:17Z",
   store: "app_store"
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
 let entitlements = {
   grace_period_expires_date: null,
-  purchase_date: "2024-04-12T01:04:17Z",
+  purchase_date: "2006-09-25T01:04:17Z",
   product_identifier: "com.locket02.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
