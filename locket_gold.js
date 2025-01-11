71e9dedcdba681eb9190859bc2bd48d866cbeb44
Phùng Đức Tài <159005177+ductaihjhj@users.noreply.github.com>
2025-01-12 01:13:27 +0700

    Update locket_gold.js

--- locket_gold.js
+++ locket_gold.js
@@ -1,46 +1,49 @@
 const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
+    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+    'Locket': ['Gold']
 };
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-    obj = JSON.parse($response.body);
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
 
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
-var locket = {
-  is_sandbox: false,
-  ownership_type: "PURCHASED",
-  billing_issues_detected_at: null,
-  period_type: "normal",
-  expires_date: "2099-12-18T01:04:17Z",
-  grace_period_expires_date: null,
-  unsubscribe_detected_at: null,
-  original_purchase_date: "2006-09-25T01:04:18Z",
-  purchase_date: "2006-09-25T01:04:17Z",
-  store: "app_store"
+var locket02 = {
+    is_sandbox: false,
+    ownership_type: "PURCHASED",
+    billing_issues_detected_at: null,
+    period_type: "normal",
+    expires_date: "2099-12-18T01:04:17Z",
+    grace_period_expires_date: null,
+    unsubscribe_detected_at: null,
+    original_purchase_date: "2006-09-25T01:04:18Z",
+    purchase_date: "2006-09-25T01:04:17Z",
+    store: "app_store"
 };
 
-const match = Object.keys(mapping).find(e => ua.includes(e));
-let entitlements = {
-  grace_period_expires_date: null,
-  purchase_date: "2006-09-25T01:04:17Z",
-  product_identifier: "com.locket02.premium.yearly",
-  expires_date: "2099-12-18T01:04:17Z"
+var locket01 = {
+    grace_period_expires_date: null,
+    purchase_date: "2006-09-25T01:04:17Z",
+    product_identifier: "com.locket02.premium.yearly",
+    expires_date: "2099-12-18T01:04:17Z"
 };
+
+const match = Object.keys(mapping).find(key => ua.includes(key));
 
 if (match) {
-  let [e, s] = mapping[match];
-  if (s) {
-    entitlements.product_identifier = s;
-    obj.subscriber.subscriptions[s] = locket;
-  } else {
-    obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket;
-  }
-  obj.subscriber.entitlements[e] = entitlements;
+    let [entitlementKey, subscriptionKey] = mapping[match];
+
+    if (subscriptionKey) {
+        locket01.product_identifier = subscriptionKey;
+        obj.subscriber.subscriptions[subscriptionKey] = locket02;
+    } else {
+        obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+    }
+
+    obj.subscriber.entitlements[entitlementKey] = locket01;
 } else {
-  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket;
-  obj.subscriber.entitlements.pro = entitlements;
+    obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+    obj.subscriber.entitlements.pro = locket01;
 }
 
 $done({ body: JSON.stringify(obj) });
